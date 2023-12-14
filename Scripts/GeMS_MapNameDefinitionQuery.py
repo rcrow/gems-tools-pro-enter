fc05f556faf9453058cebea3472eb3f893951715
Christian Halsted <christian.halsted@gmail.com>
2023-12-14 15:26:30 -0500

    Update MapNameDefinitionQuery
    updated the MapNameDefinitionQuery script to also work on tables

--- Scripts/GeMS_MapNameDefinitionQuery.py
+++ Scripts/GeMS_MapNameDefinitionQuery.py
@@ -96,7 +96,70 @@ def SetDefQuery(mapname,clear,draw):
                     cim_ft.definitionExpressionName = "MapNameDefQueryPython"
                     lyr.setDefinition(lyr_cim)
 
-        
+    for tbl in m.listTables():
+        if debug: showPyMessage(tbl.name)
+        if debug: showPyMessage(tbl.dataSource)
+        fcName=''
+        for prop in tbl.dataSource.split(','):
+            if prop[0:8] == 'Dataset=':
+                fcName = prop.replace('Dataset=','')
+        if debug: showPyMessage(fcName)
+
+        if fcName.lower().find('gems') > -1 and mapfldname in [f.name for f in arcpy.ListFields(tbl.name,'*','String')]: 
+            showPyMessage('Setting Defintion Query on ' + fcName)
+            #--clears any mapname from dq----------
+            dq = tbl.definitionQuery
+            if debug: showPyMessage('dq0: ' + str(dq))
+            dq = dq.replace('AND','And')
+            if debug: showPyMessage('dq1: ' + str(dq))
+            
+            srchstr = dq[dq.find(mapfldname + " = '"):dq.find("'",dq.find(mapfldname + " = '")+(len(mapfldname + " = '")))+1]
+            if srchstr != "'" and len(srchstr)>0:
+                dq = dq.replace(" And " + srchstr, "")
+                dq = dq.replace(srchstr + " And ", "")
+                dq = dq.replace(srchstr, "")
+            if debug: showPyMessage('dq2: ' + str(dq))
+            
+            #sets new definition query
+            if clear == "true":
+                dq = dq.replace(" And " + mapfldname + " = '" + mapname + "'", "")
+                dq = dq.replace(mapfldname + " = '" + mapname + "'" + " And ", "")
+                dq = dq.replace(mapfldname + " = '" + mapname + "'", "")
+            elif clear == "false":
+                if len((dq).strip())==0:
+                    dq = mapfldname + " = '" + mapname + "'"
+                else:
+                    if mapfldname + " = '" + mapname + "'" not in dq:
+                        dq = dq + " And " + mapfldname + " = '" + mapname + "'"
+            if debug: showPyMessage('dq3: ' + str(dq))
+            
+            if (mapfldname and drawfldname) in [f.name for f in arcpy.ListFields(tbl.name,'*','String')]:
+                if draw == "true":
+                    if drawfldname + " = 'Yes'" not in dq:
+                        if len((dq).strip())==0:
+                            dq = dq + drawfldname + " = 'Yes'"
+                        else:
+                            dq = dq + " And " + drawfldname + " = 'Yes'"
+                elif draw == "false":
+                    dq = dq.replace(" And " + drawfldname + " = 'Yes'", "")
+                    dq = dq.replace(drawfldname + " = 'Yes' And ", "")
+                    dq = dq.replace(drawfldname + " = 'Yes'", "")
+            if debug: showPyMessage('dq4: ' + str(dq))
+            
+            if debug: showPyMessage('dq: ' + str(dq))
+            
+            tbl_cim = tbl.getDefinition('V2')
+            for dfc in tbl_cim.definitionFilterChoices:
+                if dfc.name == 'MapNameDefQueryPython':
+                    dfc.definitionExpression = dq
+                else:
+                    tbl_cim.definitionFilterChoices.remove(dfc)
+                
+            tbl_cim.definitionExpression = dq
+            tbl_cim.definitionExpressionName = "MapNameDefQueryPython"
+            tbl.setDefinition(tbl_cim)
+
+  
 if __name__ == '__main__':
     mapname = arcpy.GetParameterAsText(1) #sys.argv[1]   #Map Name
     clear = arcpy.GetParameterAsText(2) #sys.argv[2]   #Clear Option
